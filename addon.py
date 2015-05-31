611460ff1706f33d5acde1ccd8e1e0fdd22bd71c
Jambonlatex <gtmicka@hotmail.fr>
2015-05-31 21:11:34 +0200

    Create addon.py

---
+++ addon.py
@@ -0,0 +1,7 @@
+"""
+    Moonlight - Add-on to launch Moonlight application from Kodi/XBMC
+"""
+
+if ( __name__ == "__main__"):
+    import resources.lib.moonlight_launcher as moonlight
+    moonlight.Launch()
